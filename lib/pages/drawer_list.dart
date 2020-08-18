f35ef1493f45c770e34f25d1d0e1985de1591d04
Raphael Costa <raphael.costa@m4u.com.br>
2020-08-18 00:49:40 -0300

    add safe area to drawer

--- lib/pages/drawer_list.dart
+++ lib/pages/drawer_list.dart
@@ -19,53 +19,55 @@ class DrawerList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      child: Column(
-        children: [
-          UserAccountsDrawerHeader(
-            accountName: Text("Pederneiras"),
-            accountEmail: Text("meuemail@email.com"),
-            arrowColor: Colors.red,
-            currentAccountPicture: CircleAvatar(
-              backgroundImage: NetworkImage(
-                  "http://br.web.img2.acsta.net/r_640_360/newsv7/19/10/04/01/13/5346274.jpg"),
+    return SafeArea(
+      child: Drawer(
+        child: Column(
+          children: [
+            UserAccountsDrawerHeader(
+              accountName: Text("Pederneiras"),
+              accountEmail: Text("meuemail@email.com"),
+              arrowColor: Colors.red,
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    "http://br.web.img2.acsta.net/r_640_360/newsv7/19/10/04/01/13/5346274.jpg"),
+              ),
             ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Text(
-            "Available pages",
-            style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.bold,
+            SizedBox(
+              height: 20,
             ),
-          ),
-          Expanded(
-            child: ListView.builder(
-              itemBuilder: (context, index) {
-                return ListTile(
-                  leading: Icon(Icons.pages),
-                  title: Text(options[index].option),
-                  subtitle: Text("Click to navigate"),
-                  trailing: Icon(Icons.arrow_right),
-                  onTap: () {
-                    Navigator.pop(context);
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) {
-                          return options[index].widget;
-                        },
-                      ),
-                    );
-                  },
-                );
-              },
-              itemCount: options.length,
+            Text(
+              "Available pages",
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
-        ],
+            Expanded(
+              child: ListView.builder(
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    leading: Icon(Icons.pages),
+                    title: Text(options[index].option),
+                    subtitle: Text("Click to navigate"),
+                    trailing: Icon(Icons.arrow_right),
+                    onTap: () {
+                      Navigator.pop(context);
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return options[index].widget;
+                          },
+                        ),
+                      );
+                    },
+                  );
+                },
+                itemCount: options.length,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
